efc791d43b2cec94fb9929958d2da02123ecaef1
Davaa1101 <Davaadavka3006@gmail.com>
2024-04-16 18:14:55 +0800

    elijah

---
+++ 2/src/bd2/Daalgavar2.java
@@ -0,0 +1,129 @@
+package bd2;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Daalgavar2{
+	static int[] Weeks = new int [16];
+		static int[] Work_days = new int [16];
+		static int[] Work_days1 = new int [16];
+		static int baga13_15 = 4 ;
+		static int dund48 = 34 ;
+		static int togsoh910_612 = 6 ;
+		static int deed1 = 3 ;
+		static int dund1_neg_tsag = 45 ;
+		static int deed_neg_tsag = 90 ;
+		static int dund2_neg_tsag_15 = 30;
+		static int dund2_neg_tsag_612 = 35 ;	
+		public int getWorkDays(int i) {
+			return Work_days[i];
+		}
+		public int getWorkDays1(int i) {
+			return Work_days1[i];
+		}
+		public int getWeeks(int i) {
+			return Weeks[i];
+		}
+		public void setWeeks() {
+			 for (int i = 1978 ; i<1994; i++) {
+				 LocalDate date1 = LocalDate.of(i, 9, 1);
+				 Weeks[i-1978] = 38;
+				 if(date1.getDayOfWeek() == DayOfWeek.MONDAY) {
+					 Weeks[i-1978] = 39 ;
+				 }
+				 else if(date1.getDayOfWeek() == DayOfWeek.SUNDAY) {
+					 if(i%4==0) {
+						 Weeks[i-1978] = 39 ;
+					 }
+				 }
+			 }
+		}
+		public void setWorkdays() {
+			for (int i = 1978 ; i<1994; i++) {
+		        LocalDate date1 = LocalDate.of(i, 9, 1); // First date
+		        LocalDate date2 = LocalDate.of(i+1, 5, 31); // Second date
+		        
+		        // Initialize counter for Mondays
+		        int mondaysCount = 0;
+		        int tuesdayCount = 0;
+		        int wednesdayCount = 0;
+		        int thursdayCount = 0;
+		        int fridayCount = 0;
+		        int saturdayCount = 0;
+		        // Iterate through the dates between date1 and date2
+		        LocalDate currentDate = date1;
+		        while (!currentDate.isAfter(date2)) {
+		            if (currentDate.getDayOfWeek() == DayOfWeek.MONDAY) {
+		                mondaysCount++;
+		            }
+		            else if (currentDate.getDayOfWeek() == DayOfWeek.TUESDAY) {
+		                tuesdayCount++;
+		            }
+		            else if (currentDate.getDayOfWeek() == DayOfWeek.WEDNESDAY) {
+		                wednesdayCount++;
+		            }
+		            else if (currentDate.getDayOfWeek() == DayOfWeek.THURSDAY) {
+		                thursdayCount++;
+		            }
+		            else if (currentDate.getDayOfWeek() == DayOfWeek.FRIDAY) {
+		                fridayCount++;
+		            } else if (currentDate.getDayOfWeek() == DayOfWeek.SATURDAY) {
+		                saturdayCount++;
+		            }
+		            // Move to the next day
+		            currentDate = currentDate.plus(1, ChronoUnit.DAYS);
+		        }
+		    Work_days[i-1978] = mondaysCount + tuesdayCount + wednesdayCount + thursdayCount + fridayCount + saturdayCount;
+		    Work_days1[i-1978] = mondaysCount + tuesdayCount + wednesdayCount + thursdayCount + fridayCount ; 
+		    }
+		}
+		public static double dund1_13(int i) {
+	        return baga13_15 * Work_days[i] * dund1_neg_tsag;
+	    }
+
+	    public static double dund1_48(int i) {
+	        return dund48 * Weeks[i] * dund1_neg_tsag;
+	    }
+
+	    public static double dund1_910(int i) {
+	        return togsoh910_612 * Work_days[i] * dund1_neg_tsag;
+	    }
+	    public static double ih1(int i) {
+	    	return (Work_days[i]-48)*deed1*deed_neg_tsag ;
+	    }
+
+	    public static double dund2_15(int i) {
+	        return baga13_15 * Work_days1[i] * dund2_neg_tsag_15;
+	    }
+
+	    public static double dund2_612(int i) {
+	        return togsoh910_612 * Work_days1[i] * dund2_neg_tsag_612;
+	    }
+
+	    public static double ih2(int i) {
+	        return deed1 * (Work_days1[i]-40) * deed_neg_tsag;
+	    }
+
+	    public static int sonirhson1(int i) {
+	        return Weeks[i] * 4* dund1_neg_tsag;
+	    }
+
+	    public static int sonirhson2(int i) {
+	            if (i < 6) {
+	                return Weeks[i] * 4 * dund2_neg_tsag_15;
+	            } else {
+	                return Weeks[i] * 4 * dund2_neg_tsag_612;
+	            }
+	        }		
+		public static void main(String[] args) {
+			Daalgavar2 daalgavar = new Daalgavar2();
+			daalgavar.setWeeks();
+			daalgavar.setWorkdays();
+			for (int i =0; i<16 ;i++) {
+				System.out.print(" "+Work_days1[i]);
+			}
+ 			
+			
+		}
+}
---
+++ 2/src/bd2/JTableExamples.java
@@ -0,0 +1,133 @@
+package bd2;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+ 
+public class JTableExamples {
+    // frame
+    JFrame f;
+    // Table
+    JTable j;
+ 
+    // Constructor
+    JTableExamples()
+    {
+    	
+         // Frame initialization
+        f = new JFrame();
+ 
+        // Frame Title
+        f.setTitle("JTable Example");
+        Daalgavar2 obj = new Daalgavar2();
+        obj.setWeeks();
+        obj.setWorkdays();
+        int[] _1978 = new int[15];
+        int[] _2006 = new int [16];
+        int count = 0, count1 = 0, counter = 0, counter1 = 0;
+ for (int i = 0; i < 15; i++) {
+	 if (i>=0 && i<3) {
+	_1978[i] = (int) obj.dund1_13(i);
+	count += (int) obj.dund1_13(i);
+	 }
+	 if (i>=3 && i<8) {
+			_1978[i] = (int) obj.dund1_48(i);
+			count += (int) obj.dund1_48(i);
+			 }
+	 if (i>=8 && i<10) {
+			_1978[i] = (int) obj.dund1_910(i);
+			count += (int) obj.dund1_910(i);
+			 }
+	 if (i>=10 && i<15) {
+			_1978[i] = (int) obj.ih1(i);
+			count1 +=(int) obj.ih1(i);
+			 }
+	 
+ }
+ for (int i = 0; i < 16; i++) {
+	 if (i>=0 && i<5) {
+	_2006[i] = (int) obj.dund2_15(i);
+	counter +=(int) obj.dund2_15(i);
+	 }
+	 if (i>=5 && i<12) {
+	_2006[i] = (int) obj.dund2_612(i);
+	counter += (int) obj.dund2_612(i);
+	 }
+	 if (i>=12 && i<16) {
+			_2006[i] = (int) obj.ih2(i);
+			counter1 += (int) obj.ih2(i);
+			 }
+ }
+ double haritsaa =(double) count/(double)count1;
+ double haritsaa1 =(double) counter/(double)counter1;
+ int num = 0;
+ for (int i = 0; i<10 ; i++) {
+	 num += obj.sonirhson1(i);
+ }
+ int num1 = 0;
+ for (int i = 0; i<12 ; i++) {
+	 num1 += obj.sonirhson2(i);
+ }
+ int ihsur1_dad = 22080*5;
+ int ihsur2_dad = 19200*4;
+        // Data to be displayed in the JTable
+        String[][] data = {
+            {"1978","бага анги",""+String.valueOf(obj.getWorkDays(0)),"24","45",""+_1978[0],""+obj.getWeeks(0), ""+obj.sonirhson1(0),""},
+            {"1979","бага анги",""+String.valueOf(obj.getWorkDays(1)),"24","45",""+_1978[1],""+obj.getWeeks(1), ""+obj.sonirhson1(1),""},
+            {"1980","бага анги",""+String.valueOf(obj.getWorkDays(2)),"24","45",""+_1978[2],""+obj.getWeeks(2), ""+obj.sonirhson1(2),""},
+            {"1981","дунд анги",""+String.valueOf(obj.getWorkDays(3)),"34","45",""+_1978[3],""+obj.getWeeks(3), ""+obj.sonirhson1(3),""},
+            {"1982","дунд анги",""+String.valueOf(obj.getWorkDays(4)),"34","45",""+_1978[4],""+obj.getWeeks(4), ""+obj.sonirhson1(4),""},
+            {"1983","дунд анги",""+String.valueOf(obj.getWorkDays(5)),"34","45",""+_1978[5],""+obj.getWeeks(5), ""+obj.sonirhson1(5),""},
+            {"1984","дунд анги",""+String.valueOf(obj.getWorkDays(6)),"34","45",""+_1978[6],""+obj.getWeeks(6), ""+obj.sonirhson1(6),""},
+            {"1985","дунд анги",""+String.valueOf(obj.getWorkDays(7)),"34","45",""+_1978[7],""+obj.getWeeks(7), ""+obj.sonirhson1(7),""},
+            {"1986","ахлах анги",""+String.valueOf(obj.getWorkDays(8)),"36","45",""+_1978[8],""+obj.getWeeks(8), ""+obj.sonirhson1(8),""},
+            {"1987","ахлах анги",""+String.valueOf(obj.getWorkDays(9)),"36","45",""+_1978[9],""+obj.getWeeks(9), ""+obj.sonirhson1(9),""},
+            {"1988","дээд сургууль",""+String.valueOf(obj.getWorkDays(10)),"18","90",""+_1978[10],""+obj.getWeeks(10),"", "22080"},
+            {"1989","дээд сургууль",""+String.valueOf(obj.getWorkDays(11)),"18","90",""+_1978[11],""+obj.getWeeks(11),"", "22080"},
+            {"1990","дээд сургууль",""+String.valueOf(obj.getWorkDays(12)),"18","90",""+_1978[12],""+obj.getWeeks(12),"", "22080"},
+            {"1991","дээд сургууль",""+String.valueOf(obj.getWorkDays(13)),"18","90",""+_1978[13],""+obj.getWeeks(13),"", "22080"},
+            {"1992","дээд сургууль",""+String.valueOf(obj.getWorkDays(14)),"18","90",""+_1978[14],""+obj.getWeeks(14),"", "22080"},  
+            {"2006","бага анги",""+String.valueOf(obj.getWorkDays1(0)),"20","30",""+_2006[0],""+obj.getWeeks(0), ""+obj.sonirhson2(0),""},
+            {"2007","бага анги",""+String.valueOf(obj.getWorkDays1(1)),"20","30",""+_2006[1],""+obj.getWeeks(1), ""+obj.sonirhson2(1),""},
+            {"2008","бага анги",""+String.valueOf(obj.getWorkDays1(2)),"20","30",""+_2006[2],""+obj.getWeeks(2), ""+obj.sonirhson2(2),""},
+            {"2009","бага анги",""+String.valueOf(obj.getWorkDays1(3)),"20","30",""+_2006[3],""+obj.getWeeks(3), ""+obj.sonirhson2(3),""},
+            {"2010","бага анги",""+String.valueOf(obj.getWorkDays1(4)),"20","30",""+_2006[4],""+obj.getWeeks(4), ""+obj.sonirhson2(4),""},
+            {"2011","ахлах анги",""+String.valueOf(obj.getWorkDays1(5)),"30","35",""+_2006[5],""+obj.getWeeks(5), ""+obj.sonirhson2(5),""},
+            {"2012","ахлах анги",""+String.valueOf(obj.getWorkDays1(6)),"30","35",""+_2006[6],""+obj.getWeeks(6), ""+obj.sonirhson2(6),""},
+            {"2013","ахлах анги",""+String.valueOf(obj.getWorkDays1(7)),"30","35",""+_2006[7],""+obj.getWeeks(7), ""+obj.sonirhson2(7),""},
+            {"2014","ахлах анги",""+String.valueOf(obj.getWorkDays1(8)),"30","35",""+_2006[8],""+obj.getWeeks(8), ""+obj.sonirhson2(8),""},
+            {"2015","ахлах анги",""+String.valueOf(obj.getWorkDays1(9)),"30","35",""+_2006[9],""+obj.getWeeks(9), ""+obj.sonirhson2(9),""},
+            {"2016","ахлах анги",""+String.valueOf(obj.getWorkDays1(10)),"30","35",""+_2006[10],""+obj.getWeeks(10), ""+obj.sonirhson2(10),""},
+            {"2017","ахлах анги",""+String.valueOf(obj.getWorkDays1(11)),"30","35",""+_2006[11],""+obj.getWeeks(11), ""+obj.sonirhson2(11),""},
+            {"2018","дээд сургууль",""+String.valueOf(obj.getWorkDays1(12)),"15","90",""+_2006[12],""+obj.getWeeks(12),"", "19200"},
+            {"2019","дээд сургууль",""+String.valueOf(obj.getWorkDays1(13)),"15","90",""+_2006[13],""+obj.getWeeks(13),"", "19200"},
+            {"2020","дээд сургууль",""+String.valueOf(obj.getWorkDays1(14)),"15","90",""+_2006[14],""+obj.getWeeks(14),"", "19200"},
+            {"2021","дээд сургууль",""+String.valueOf(obj.getWorkDays1(15)),"15","90",""+_2006[15],""+obj.getWeeks(15),"", "19200"},
+            {"1р тохиолдолд дунд сургууль","1р тохиолдолд дээд сургууль","2р тохиолдолд дунд сургууль","2р тохиолдолд дээд сургууль","1р харьцаа","2р харьцаа","", "",""},
+            {""+count,""+count1,""+counter,""+counter1,""+String.valueOf(haritsaa),""+String.valueOf(haritsaa1),"", "",""},
+            {"1р тохиолдолд сонирхсон","2р тохиолдолд сонирхсон","Харьцаа","1р тохиолдолд нийт дадлага","2р тохиолдолд нийт дадлага/Дээд сургуульд сурсан хугацаа","2р тохиолдолд нийт дадлага", "2р тохиолдолд нийт дадлага/Дээд сургуульд сурсан хугацаа","",""},
+            {""+num,""+num1,""+(double) num/num1,""+ihsur1_dad,""+(double) ihsur1_dad/count1*100+"%",""+ihsur2_dad, ""+(double) ihsur2_dad/counter1*100+"%","",""},
+                    };
+ 
+        // Column Names
+        String[] columnNames = { "он", "төлөв","Хичээлийн өдөр","7 хоногт орох цаг","нэг цаг үргэлжлэх хугацаа","нийт(минут)" ,"Бүтэн 7 хоногийн тоо","Сонирхсон хичээл", "Дадлага"};
+ 
+        // Initializing the JTable
+        j = new JTable(data, columnNames);
+        j.setBounds(30, 40, 200, 300);
+ 
+        // adding it to JScrollPane
+        JScrollPane sp = new JScrollPane(j);
+        f.add(sp);
+        // Frame Size
+        f.setSize(500, 200);
+        // Frame Visible = true
+        f.setVisible(true);
+    }
+ 
+    // Driver  method
+    public static void main(String[] args)
+    {
+        new JTableExamples();
+    }
+}
+
